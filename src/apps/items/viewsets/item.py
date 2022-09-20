10c8ba64ded12a8429c79419b29e14eec9bea2a0
Kirill Mil <kirill.mil.0310@mail.ru>
2022-09-20 18:20:10 +0700

    add publish api key

--- src/apps/items/viewsets/item.py
+++ src/apps/items/viewsets/item.py
@@ -1,3 +1,5 @@
+import os
+
 from rest_framework import viewsets, mixins
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
@@ -11,4 +13,10 @@ class ItemViewSet(viewsets.GenericViewSet, mixins.RetrieveModelMixin):
     template_name = 'detail.html'
 
     def retrieve(self, request, *args, **kwargs):
-        return Response({'item': self.get_object()}, template_name='items_detail.html')
+        return Response(
+            {
+                'item': self.get_object(),
+                'stripe_publish_key': os.environ.get('STRIPE_API_PUBLISH_KEY')
+            },
+            template_name='items_detail.html'
+        )
